c9063ce2d017cb55f29e18f9ae745eaea4387b41
Kent Delante <leftybournes@pm.me>
2022-02-06 11:59:12 +0800

    This is where the fun begins

---
+++ README.md
@@ -0,0 +1,8 @@
+# Monetrail
+
+Budget, track and sync your finances with your loved ones.
+
+## License
+
+Monetrail is licensed under the GNU General Public License version 3.0 or later
+(GNU GPL-3.0-or-later).
---
+++ app/src/main/java/com/kylobytes/monetrail/ui/theme/Type.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2022 Kent Delante  <leftybournes@pm.me>.
+ *
+ * This file is part of Monetrail.
+ *
+ * Monetrail  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Monetrail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Monetrail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.kylobytes.monetrail.ui.theme
+
+import androidx.compose.material.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
