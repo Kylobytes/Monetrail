fd4539e060576ffd1f3a6577f58f400b9db18a49
Kent Delante <leftybournes@pm.me>
2022-02-07 19:27:25 +0800

    Setup skeleton project

--- app/src/main/java/com/kylobytes/monetrail/MainActivity.kt
+++ app/src/main/java/com/kylobytes/monetrail/MainActivity.kt
@@ -30,7 +30,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.kylobytes.monetrail.ui.theme.MonetrailTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/kylobytes/monetrail/data/Database.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) 2022 Kent Delante  <leftybournes@pm.me>.
+ *
+ * This file is part of Monetrail.
+ *
+ * Monetrail  is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Monetrail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Monetrail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.kylobytes.monetrail.data
+
+import androidx.room.RoomDatabase
+
+abstract class Database : RoomDatabase() {
+}
